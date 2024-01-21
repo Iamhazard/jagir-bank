d4efd3e1813d77df8aa197b1413602bf80c320db
Amrit Acharya <Amrithazard@gmail.com>
2024-01-21 22:10:27 +0545

    fixes design

--- Components/auth/Guest.tsx
+++ Components/auth/Guest.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import styles from "../../styles/combined.module.css";
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
+import { Card } from "../ui/card";
 
 const GuestLogin = () => {
   const [selectedOption, setSelectedOption] = useState<string>("");
@@ -24,17 +25,17 @@ const GuestLogin = () => {
     }
   };
   return (
-    <section className="mt-40  pb-10">
-      <div className={styles.card}>
-        <div className={styles.tools}>
-          <h1 className="flex justify-center ">
-            Join as a client or freelancer
-          </h1>
-        </div>
-        <div className="grid ml-6 items-center mt-14 grid-cols-1 gap-2 sm:grid-cols-2 ">
+    <Card>
+      <div className={styles.tools}>
+        <h1 className="flex justify-center mt-4 ">
+          Join as a client or freelancer
+        </h1>
+      </div>
+      <div className="grid ml-6 items-center mt-6 grid-cols-1 gap-2 sm:grid-cols-2 ">
+        <Card>
           <div
             onClick={OnClientClick}
-            className="h-36 w-60 py-2 px-2 nested-card group border-[2px] border-gray-100  gap-2 rounded-4xl shadow-md transition-all duration-300 ease-in-out hover:border-green-700">
+            className=" py-2 px-2 nested-card group border-[2px] border-gray-100  gap-2 rounded-4xl shadow-md transition-all duration-300 ease-in-out hover:border-green-700">
             <label className="flex mb-8 cursor-pointer">
               <input type="checkbox" className="hidden" />
               <span className="w-6 h-6 border border-gray-700 rounded-full relative transition duration-300 group-hover:bg-green-600 checked:bg-green-600">
@@ -46,9 +47,11 @@ const GuestLogin = () => {
               I’m a client, hiring for a project
             </span>
           </div>
+        </Card>
+        <Card className="mr-3">
           <div
             onClick={OnFreeLanceClick}
-            className="h-36 w-60  py-2 px-2 nested-card group  border-[2px]  border-gray-100  gap-2 rounded-4xl shadow-md transition-all duration-300 ease-in-out hover:border-green-700">
+            className="py-2 px-2 nested-card group  border-[2px]  border-gray-100  gap-2 rounded-4xl shadow-md transition-all duration-300 ease-in-out hover:border-green-700">
             <label className="flex mb-8  cursor-pointer">
               <input type="checkbox" className="hidden" />
               <span className="w-6 h-6  border  border-gray-700 rounded-full relative transition duration-300 group-hover:bg-green-700 checked:bg-green-600">
@@ -59,25 +62,25 @@ const GuestLogin = () => {
               I’m a freelancer, looking for work
             </span>
           </div>
-          <div>
-            <button className={styles.btn} onClick={handleClick}>
-              {selectedOption === "Client"
-                ? "Sign up as Client"
-                : selectedOption === "Freelancer"
-                ? "Sign up as Freelancer"
-                : "Create Account"}
-            </button>
+        </Card>
+        <div>
+          <button className={styles.btn} onClick={handleClick}>
+            {selectedOption === "Client"
+              ? "Sign up as Client"
+              : selectedOption === "Freelancer"
+              ? "Sign up as Freelancer"
+              : "Create Account"}
+          </button>
 
-            <p className="justify-center">
-              Already have an Account?{" "}
-              <Link className="text-sm text-green-800" href="auth/login">
-                Login
-              </Link>
-            </p>
-          </div>
+          <p className="justify-center">
+            Already have an Account?{" "}
+            <Link className="text-sm text-green-800" href="auth/login">
+              Login
+            </Link>
+          </p>
         </div>
       </div>
-    </section>
+    </Card>
   );
 };
 
