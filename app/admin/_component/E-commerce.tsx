25ad90662b82f38e1d774bdd15bec94347c6298e
Amrit Acharya <Amrithazard@gmail.com>
2024-07-09 21:02:03 +0545

    na

--- app/admin/_component/E-commerce.tsx
+++ app/admin/_component/E-commerce.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import ChartOne from "./Charts/ChartOne";
 import ChartTwo from "./Charts/ChartTwo";
 import ChartThree from "./Charts/ChartThree";
-import MapOne from "./Maps/MapOne";
+//import MapOne from "./Maps/MapOne";
 import TableOne from "./Tables/TableOne";
 import ChatCard from "./Chat/ChatCard";
 import CardDataStats from "./CardDataStats";
@@ -104,7 +104,7 @@ const ECommerce: React.FC = () => {
                 <ChartOne />
                 <ChartTwo />
                 <ChartThree />
-                <MapOne />
+                {/* <MapOne /> */}
                 <div className="col-span-12 xl:col-span-8">
                     <TableOne />
                 </div>
