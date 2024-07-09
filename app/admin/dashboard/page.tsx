7e9e640a533acbd1e8e848be046288768890053d
Amrit Acharya <Amrithazard@gmail.com>
2024-07-09 20:33:36 +0545

    ....

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,23 +1,11 @@
 "use client"
 
 import React, { useEffect } from "react";
-import { admin } from "@/actions/admin";
 import toast from "react-hot-toast";
 import ECommerce from "../_component/E-commerce";
 import withAdmin from "../_component/AdminOrder";
 
 const DefaultDashboard = () => {
-    const onServerActionClick = () => {
-        admin().then((data) => {
-            if (data.error) {
-                toast.error(data.error);
-            }
-
-            if (data.success) {
-                toast.success(data.success);
-            }
-        });
-    };
 
     useEffect(() => {
         const onApiRouteClick = () => {
